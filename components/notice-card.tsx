1c89ec35471fb9a8f2dbe2c9ce7ab86776bc0a59
Milan6299 <milanmahapatra140@gmail.com>
2026-02-28 21:23:57 +0530

    made the badge fit content

--- components/notice-card.tsx
+++ components/notice-card.tsx
@@ -1,45 +1,47 @@
 import {
-  Card,
-  CardContent,
-  CardFooter,
-  CardHeader,
-  CardTitle,
+	Card,
+	CardContent,
+	CardFooter,
+	CardHeader,
+	CardTitle,
 } from "@/components/ui/card";
 import { Badge } from "@/components/ui/badge";
 import { Button } from "./ui/button";
 import Link from "next/link";
 
 const notices = [
-  "Dinner delayed by 30 minutes",
-  "Special sweets on Friday",
-  "Kitchen maintenance on Sunday",
+	"Dinner delayed by 30 minutes",
+	"Special sweets on Friday",
+	"Kitchen maintenance on Sunday",
 ];
 
 const NoticesCard = () => {
-  return (
-    <Card>
-      <CardHeader>
-        <div className="flex items-center justify-between">
-          <CardTitle>Notices</CardTitle>
-        </div>
-      </CardHeader>
-      <CardContent className="space-y-2">
-        {notices.map((note, i) => (
-          <div key={i} className="flex justify-between text-sm gap-2">
-            <span>{note}</span>
-            <Badge variant="secondary">New</Badge>
-          </div>
-        ))}
-      </CardContent>
-      <CardFooter className="mt-auto">
-        <Link href={"/student/notices"} className="ml-auto">
-          <Button size={"responsive"} variant={"info"}>
-            View
-          </Button>
-        </Link>
-      </CardFooter>
-    </Card>
-  );
+	return (
+		<Card>
+			<CardHeader>
+				<div className="flex items-center justify-between">
+					<CardTitle>Notices</CardTitle>
+				</div>
+			</CardHeader>
+			<CardContent className="space-y-2">
+				{notices.map((note, i) => (
+					<div key={i} className="flex justify-between text-sm gap-2">
+						<span>{note}</span>
+						<Badge variant="secondary" className="max-h-max">
+							New
+						</Badge>
+					</div>
+				))}
+			</CardContent>
+			<CardFooter className="mt-auto">
+				<Link href={"/student/notices"} className="ml-auto">
+					<Button size={"responsive"} variant={"info"}>
+						View
+					</Button>
+				</Link>
+			</CardFooter>
+		</Card>
+	);
 };
 
 export default NoticesCard;
