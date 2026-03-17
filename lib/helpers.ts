6faa3f11e80eee572f919bf00831e5864b660f48
Milan6299 <milanmahapatra140@gmail.com>
2026-03-18 00:20:07 +0530

    removed redundant helpers

--- lib/helpers.ts
+++ lib/helpers.ts
@@ -1,35 +1,8 @@
 import axios from "axios";
 
-export function getCookie(name: string) {
-	let cookieValue = null;
-
-	if (document.cookie && document.cookie !== "") {
-		const cookies = document.cookie.split(";");
-
-		for (let cookie of cookies) {
-			cookie = cookie.trim();
-
-			if (cookie.startsWith(name + "=")) {
-				cookieValue = decodeURIComponent(cookie.slice(name.length + 1));
-				break;
-			}
-		}
-	}
-
-	return cookieValue;
-}
-
 export const api = axios.create({
 	baseURL: "http://localhost:8000",
 	withCredentials: true,
-});
-
-api.interceptors.request.use((config) => {
-	const csrftoken = getCookie("csrftoken");
-
-	if (csrftoken) {
-		config.headers["X-CSRFToken"] = csrftoken;
-	}
-
-	return config;
+	xsrfCookieName: "csrftoken",
+	xsrfHeaderName: "X-CSRFToken",
 });
