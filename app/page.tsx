28f75d38bca2cc8dd5b27007a8e2a3211965a112
Milan6299 <milanmahapatra140@gmail.com>
2026-03-18 00:56:53 +0530

    if user role is not found then redirect to signup

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default async function Home() {
 	// const user = getCurrentUser();
 	const user = { role: null };
 
-	if (!user) redirect("/login");
+	if (!user.role) redirect("/signup");
 
 	if (user.role === "student") redirect("/student");
 	if (user.role === "cook") redirect("/cook");
