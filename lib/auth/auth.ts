572df7c7d7733f30a10ee8088845a617aceb5230
Milan6299 <milanmahapatra140@gmail.com>
2026-03-18 00:20:32 +0530

    added function to get details of current user

--- lib/auth/auth.ts
+++ lib/auth/auth.ts
@@ -1,3 +1,11 @@
-export function getCurrentUser() {
-  return { role: "student" };
+import { api } from "../helpers";
+
+export async function getCurrentUser() {
+	try {
+		const response = api.get(`/api/authenticate/`);
+		return response;
+	} catch (err) {
+		console.log(err);
+	}
+	// return { role: "student" };
 }
