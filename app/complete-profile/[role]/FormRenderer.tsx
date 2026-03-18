32eb6b9f6b4beee3fe4dfe68531f2fcfc5f209a9
Milan6299 <milanmahapatra140@gmail.com>
2026-03-18 23:19:23 +0530

    renamed ProfileForm to FormRenderer as it renders forms based on user's role

---
+++ app/complete-profile/[role]/FormRenderer.tsx
@@ -0,0 +1,41 @@
+"use client";
+import { api } from "@/lib/helpers";
+import { type ReactElement } from "react";
+
+import { useEffect } from "react";
+import { StudentProfileForm } from "./StudentForm";
+
+export default function FormRenderer({ role }: { role: string }): ReactElement {
+	const forms = [
+		{
+			role: "student",
+			render: () => <StudentProfileForm />,
+		},
+		{
+			role: "cook",
+			render: () => <StudentProfileForm />,
+		},
+	];
+
+	const currentForm = forms.filter((f) => f.role === role);
+
+	async function get_profile_data() {
+		const response = await api
+			.get(`/api/get_profile/`)
+			.then((data) => console.log(data))
+			.catch((err) => console.log(err));
+		console.log(response);
+	}
+	useEffect(() => {
+		get_profile_data();
+	});
+
+	return (
+		<div>
+			<h1 className="text-center">{role} Profile Form</h1>
+			<div className="w-full flex justify-center">
+				{currentForm[0].render()}
+			</div>
+		</div>
+	);
+}
