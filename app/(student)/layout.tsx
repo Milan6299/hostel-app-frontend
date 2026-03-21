0202a679bf9acac69a3637b22044df364f0ed8a1
Milan6299 <milanmahapatra140@gmail.com>
2026-03-21 09:47:52 +0530

    updated app name in layout (student)

--- app/(student)/layout.tsx
+++ app/(student)/layout.tsx
@@ -3,18 +3,18 @@ import { AppSidebar } from "@/components/app-sidebar";
 import Link from "next/link";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
-  const role = "student";
-  return (
-    <SidebarProvider>
-      <AppSidebar role={role} />
+	const role = "student";
+	return (
+		<SidebarProvider>
+			<AppSidebar role={role} />
 
-      <main className="flex-1 px-4 pb-4">
-        <div className="flex sticky z-50 top-0 py-4 bg-background items-center mb-4 gap-2">
-          <SidebarTrigger />
-          <Link href={"#"}>Hostel App</Link>
-        </div>
-        {children}
-      </main>
-    </SidebarProvider>
-  );
+			<main className="flex-1 px-4 pb-4">
+				<div className="flex sticky z-50 top-0 py-4 bg-background items-center mb-4 gap-2">
+					<SidebarTrigger />
+					<Link href={"#"}>FMU Mess</Link>
+				</div>
+				{children}
+			</main>
+		</SidebarProvider>
+	);
 }
