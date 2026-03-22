d0b19b8fb304ab1285ff75694c56f8db4dc54905
Milan6299 <milanmahapatra140@gmail.com>
2026-03-22 23:39:13 +0530

    styling changes

--- components/skeleton-form.tsx
+++ components/skeleton-form.tsx
@@ -2,7 +2,7 @@ import { Skeleton } from "./ui/skeleton";
 
 export function SkeletonForm() {
 	return (
-		<div className="flex w-full max-w-xs flex-col gap-7">
+		<div className="grid w-full sm:max-w-lg gap-7">
 			<div className="flex flex-col gap-3">
 				<Skeleton className="h-4 w-20" />
 				<Skeleton className="h-8 w-full" />
