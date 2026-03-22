e38e5e9ba5ee1a287e98af6f3f39a79c9a992984
Milan6299 <milanmahapatra140@gmail.com>
2026-03-22 22:29:37 +0530

    style changes

--- components/today-meal-card.tsx
+++ components/today-meal-card.tsx
@@ -3,61 +3,57 @@ import { Check, Circle, X } from "lucide-react";
 import { Badge } from "./ui/badge";
 import { Button } from "./ui/button";
 import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
+	Card,
+	CardContent,
+	CardDescription,
+	CardFooter,
+	CardHeader,
+	CardTitle,
 } from "./ui/card";
 import { useState } from "react";
 
 const TodayMealCard = () => {
-  const [preference, setPreference] = useState<boolean>(true);
-  const currentMeal = {
-    type: "Lunch",
-    nvegitem: "Rice, Dal, Chicken, Salad",
-    vegitem: "Rice, Dal, Paneer, Salad",
-    special: true,
-  };
-  return (
-    <Card className="">
-      <CardHeader>
-        <CardTitle>
-          {"Today's"} {currentMeal.type}
-        </CardTitle>
-      </CardHeader>
+	const [preference, setPreference] = useState<boolean>(true);
+	const currentMeal = {
+		type: "Lunch",
+		nvegitem: "Rice, Dal, Chicken, Salad",
+		vegitem: "Rice, Dal, Paneer, Salad",
+		special: true,
+	};
+	return (
+		<Card className="">
+			<CardContent className="py-0">
+				<CardDescription className="space-y-2">
+					<h2 className="flex justify-between font-bold text-lg">
+						<strong className="text-foreground">{currentMeal.type}</strong>
 
-      <CardContent className="">
-        <CardDescription>
-          <Badge variant={"default"}>
-            {currentMeal.special ? "Special" : "Normal"}
-          </Badge>
-
-          <p className="mt-2">
-            {preference ? currentMeal.nvegitem : currentMeal.vegitem}
-          </p>
-        </CardDescription>
-      </CardContent>
-      <CardFooter className="flex flex-row-reverse gap-1 md:gap-2 mt-auto ml-auto">
-        <Button variant={"success"} size={"responsive"}>
-          <Check />
-        </Button>
-        <Button variant={"destructive"} size={"responsive"}>
-          <X />
-        </Button>
-        <Button
-          onClick={() => setPreference(!preference)}
-          variant={"secondary"}
-          size={"responsive"}
-        >
-          <Circle
-            className={`${preference ? "bg-destructive" : "bg-success"} text-secondary rounded-full`}
-          />
-        </Button>
-      </CardFooter>
-    </Card>
-  );
+						<Badge variant={"default"}>
+							{currentMeal.special ? "Special" : "Normal"}
+						</Badge>
+					</h2>
+					<p className="mt-2">
+						{preference ? currentMeal.nvegitem : currentMeal.vegitem}
+					</p>
+				</CardDescription>
+			</CardContent>
+			<CardFooter className="flex flex-row-reverse gap-1 md:gap-2 mt-auto ml-auto">
+				<Button variant={"success"}>
+					<Check />
+				</Button>
+				<Button variant={"destructive"}>
+					<X />
+				</Button>
+				<Button
+					onClick={() => setPreference(!preference)}
+					variant={"secondary"}
+				>
+					<Circle
+						className={`${preference ? "bg-destructive" : "bg-success"} text-secondary rounded-full`}
+					/>
+				</Button>
+			</CardFooter>
+		</Card>
+	);
 };
 
 export default TodayMealCard;
