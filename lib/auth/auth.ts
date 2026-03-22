31b6fec5addf8926dc1ed6df8a0a070d0cad1197
Milan6299 <milanmahapatra140@gmail.com>
2026-03-22 17:10:30 +0530

    better api error handling for all api and updated npm

--- lib/auth/auth.ts
+++ lib/auth/auth.ts
@@ -3,44 +3,79 @@ import { api } from "../helpers";
 import { StudentProfileSchema } from "@/app/complete-profile/[role]/StudentForm";
 import { SignupSchema } from "@/app/signup/signup-form";
 
-export async function loginUser(data: LoginSchema) {
-	try {
-		const response = await api.post("/api/login/", data);
-		console.log(response);
-		return response;
-	} catch (err: unknown) {
-		console.log(err.response.data);
-		throw err;
-	}
+type ApiError = {
+	status: number;
+	error: string;
+	code?: string;
+	role?: string;
+};
+
+// Reusable error normalizer
+function handleApiError(err: any): ApiError {
+	return {
+		status: err?.response?.status,
+		error: err?.response?.data?.error || "Something went wrong",
+		code: err?.response?.data?.code,
+		role: err?.response?.data?.role,
+	};
 }
 
+// Signup
 export async function signupUser(data: SignupSchema) {
 	try {
 		const response = await api.post("/api/signup/", data);
-		return response;
-	} catch (err) {
-		console.log(err);
-		throw err;
+		return response.data;
+	} catch (err: any) {
+		throw handleApiError(err);
 	}
 }
 
+// Login
+export async function loginUser(data: LoginSchema) {
+	try {
+		const response = await api.post("/api/login/", data);
+		return response.data;
+	} catch (err: any) {
+		throw handleApiError(err);
+	}
+}
+
+// Logout
+
+export async function logoutUser() {
+	try {
+		const resp = await api.post("/api/logout/");
+		return resp.data;
+	} catch (err: any) {
+		throw handleApiError(err);
+	}
+}
+// Complete Profile
 export async function completeProfile(data: StudentProfileSchema) {
 	try {
 		const response = await api.post("/api/complete_profile/", data);
-		return response;
-	} catch (err) {
-		console.log(err.response.data);
-		throw err;
+		return response.data;
+	} catch (err: any) {
+		throw handleApiError(err);
 	}
 }
 
-export async function getCurrentUser() {
+// Authenticate
+export async function checkAuth() {
 	try {
 		const response = await api.get("/api/authenticate/");
-		console.log("response", response.data);
-		return response;
-	} catch (err) {
-		console.log(err);
-		throw err;
+		return response.data;
+	} catch (err: any) {
+		throw handleApiError(err);
+	}
+}
+
+// Get Profile
+export async function getProfile() {
+	try {
+		const response = await api.get("/api/profile");
+		return response.data;
+	} catch (err: any) {
+		throw handleApiError(err);
 	}
 }
