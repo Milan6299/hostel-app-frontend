ddc2676e969807b020ebec9bb2eb312605c8cdc4
Milan6299 <milanmahapatra140@gmail.com>
2026-04-14 13:01:28 +0530

    made default theme dark for now and added notice provider

--- app/page.tsx
+++ app/page.tsx
@@ -4,21 +4,17 @@ import { redirect } from "next/navigation";
 export default async function Home() {
 	const user = await getCurrentUserServer();
 
-	// 1. If no user, go to login
 	if (!user) {
 		redirect("/login");
 	}
 
-	// 2. If user exists but profile isn't done
 	if (!user.profile_complete) {
 		redirect(`/complete-profile/${user.role}`);
 	}
 
-	// 3. Role-based routing
 	if (user.role === "student") redirect("/student");
 	if (user.role === "cook") redirect("/cook");
 	if (user.role === "admin") redirect("/admin");
 
-	// Fallback
 	return null;
 }
--- app/providers.tsx
+++ app/providers.tsx
@@ -3,7 +3,29 @@ import { type ReactElement } from "react";
 import { useEffect } from "react";
 import { api } from "@/lib/helpers";
 import { authUrl } from "@/lib/auth/auth";
+import { useNoticeStore } from "@/lib/store/useNoticeStore";
 
+export function NoticeProvider({
+	children,
+}: {
+	children: React.ReactNode;
+}): ReactElement {
+	const { fetchUnreadCount } = useNoticeStore();
+	useEffect(() => {
+		const fetchNotice = async () => {
+			try {
+				await fetchUnreadCount();
+			} catch (err) {
+				console.error("Failed!", err);
+				alert("Unable to fetch Notices!");
+			}
+		};
+
+		fetchNotice();
+	}, []);
+
+	return <>{children}</>;
+}
 export default function CSRFProvider({
 	children,
 }: {
