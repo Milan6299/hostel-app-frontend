f81925fc2b630f3d3470697a42b24632cc0c8f9f
Milan6299 <milanmahapatra140@gmail.com>
2026-04-14 12:57:16 +0530

    added cook layout

---
+++ app/(cook)/layout.tsx
@@ -0,0 +1,49 @@
+import { AppSidebar } from "@/components/app-sidebar";
+import LogoBtn from "@/components/LogoBtn";
+import MobileDock from "@/components/MobileDock";
+import ProfileDropdown from "@/components/ProfileDropDown";
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import { type ReactElement } from "react";
+
+export default function CookLayout({
+	children,
+}: {
+	children: React.ReactNode;
+}): ReactElement {
+	const role = "cook";
+	return (
+		<div>
+			<div className="min-h-screen">
+				<div className="sm:hidden">
+					<main className="flex-1 px-4 py-24">
+						<div className="flex fixed w-screen h-20 z-50 top-0 left-0 p-4 bg-background items-center mb-4 gap-2 border-b">
+							<LogoBtn />
+							<div className="ml-auto">
+								<ProfileDropdown role={role} />
+							</div>
+						</div>
+						{children}
+						<MobileDock role={role} />
+					</main>
+				</div>
+
+				{/* Desktop View */}
+				<div className="hidden sm:block">
+					<SidebarProvider>
+						<AppSidebar role={role} />
+						<main className="flex-1 px-4 pb-4">
+							<div className="flex sticky z-50 top-0 py-4 bg-background items-center mb-4 gap-2">
+								<SidebarTrigger />
+								<LogoBtn />
+								<div className="ml-auto">
+									<ProfileDropdown role={role} />
+								</div>
+							</div>
+							{children}
+						</main>
+					</SidebarProvider>
+				</div>
+			</div>
+		</div>
+	);
+}
