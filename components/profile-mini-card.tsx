3b800de0c71427b19e43855e996b8e231a38159b
Milan6299 <milanmahapatra140@gmail.com>
2026-04-15 01:05:37 +0530

    dynamic data profile mini card

--- components/profile-mini-card.tsx
+++ components/profile-mini-card.tsx
@@ -9,7 +9,7 @@ import {
 import Image from "next/image";
 import { Button } from "./ui/button";
 import Link from "next/link";
-import { Bed, BookMarked, Hotel, User } from "lucide-react";
+import { Bed, BookMarked, Hotel, IdCard, Phone, User } from "lucide-react";
 import { useEffect, useState } from "react";
 import { getProfile } from "@/lib/auth/auth";
 import { toast } from "sonner";
@@ -19,6 +19,7 @@ interface Profile {
 	first_name: string;
 	last_name: string;
 	department: string;
+	roll_no: string;
 	room_no: number;
 	hostel_type: string;
 	hostel_block: string;
@@ -52,52 +53,60 @@ const ProfileMiniCard = () => {
 	if (isLoading || !proData) return <SkeletonCard />;
 
 	return (
-		<Card className="relative overflow-hidden">
-			<CardContent>
-				<div className="grid h-full gap-4">
-					<div className="flex gap-4 items-center justify-center">
-						<div className="relative h-32 aspect-square overflow-hidden rounded-full">
-							<Image fill src={"/profile.svg"} alt="Profile" />
-						</div>
-					</div>
-					<CardTitle className="font-bold text-center flex justify-center items-center ">
-						<div className="text-center text-sm md:text-base flex items-center bg-foreground rounded-full px-4 text-background leading-relaxed">
-							{proData.first_name} {proData.last_name}
-						</div>
-					</CardTitle>
-
-					<CardTitle className="font-bold text-center flex justify-center items-center ">
-						<div className="text-center text-sm md:text-base flex items-center bg-foreground rounded-full px-4 text-background leading-relaxed">
-							<BookMarked className="w-5 h-4" /> {proData.department}
-						</div>
-					</CardTitle>
-					<div className="">
-						<CardDescription>
-							<div className="flex mt-4 justify-center items-center gap-4">
-								<div className="flex gap-2 justify-center items-center text-lg">
-									<Hotel className="w-5 h-5" />{" "}
-									<span>{proData.hostel_block}</span>
-								</div>
-								<div className="flex justify-center items-center gap-2">
-									<Bed /> {proData.room_no}
-								</div>
-								<div className="flex justify-center items-center gap-2">
-									<User /> {proData.hostel_type === "boys" ? "BOYS" : "GIRLS"}
-								</div>
+		<Link href={"/student/profile/"} className="flex">
+			<Card className="relative overflow-hidden w-full">
+				<CardContent className="items-center h-full">
+					<div className="grid h-full gap-2">
+						<div className="flex gap-4 items-center justify-center">
+							<div className="relative h-32 aspect-square overflow-hidden rounded-full">
+								<Image fill src={"/profile.svg"} alt="Profile" />
 							</div>
+						</div>
+						<CardTitle className="font-bold text-center flex justify-center items-center ">
+							<div className="text-center text-sm md:text-base flex items-center bg-foreground rounded-full px-4 text-background leading-relaxed">
+								{proData.first_name} {proData.last_name}
+							</div>
+						</CardTitle>
 
-							<p className="text-center mt-2">+91 {proData.phone}</p>
-						</CardDescription>
+						<CardTitle className="font-bold text-center flex justify-center items-center ">
+							<div className="text-center text-sm md:text-base flex items-center bg-foreground rounded-full px-4 text-background leading-relaxed">
+								<BookMarked className="w-5 h-4" /> {proData.department}
+							</div>
+						</CardTitle>
+						<div className="">
+							<CardDescription>
+								<div className="flex justify-center items-center gap-4">
+									<IdCard />
+									{proData.roll_no}
+								</div>
+
+								<div className="text-center mt-2 flex gap-4 items-center justify-center">
+									<Phone /> +91 {proData.phone}
+								</div>
+								<div className="flex mt-4 justify-center items-center gap-4">
+									<div className="flex gap-2 justify-center items-center text-lg">
+										<Hotel className="w-5 h-5" />{" "}
+										<span>{proData.hostel_block}</span>
+									</div>
+									<div className="flex justify-center items-center gap-2">
+										<Bed /> {proData.room_no}
+									</div>
+									<div className="flex justify-center items-center gap-2">
+										<User /> {proData.hostel_type === "boys" ? "BOYS" : "GIRLS"}
+									</div>
+								</div>
+							</CardDescription>
+						</div>
+
+						{
+							// <Link href="/student/profile" className="w-full flex justify-center">
+							// 	<Button variant={"info"}>Profile</Button>
+							// </Link>
+						}
 					</div>
-				</div>
-			</CardContent>
-
-			<CardFooter>
-				<Link href="/student/profile" className="w-full flex justify-center">
-					<Button variant={"info"}>Profile</Button>
-				</Link>
-			</CardFooter>
-		</Card>
+				</CardContent>
+			</Card>
+		</Link>
 	);
 };
 
