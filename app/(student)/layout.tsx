e071aa6d7bda70e985424bb4cdc0dded533aa921
Milan6299 <milanmahapatra140@gmail.com>
2026-04-14 12:57:56 +0530

    had client imports in layout separated them to specific components

--- app/(student)/layout.tsx
+++ app/(student)/layout.tsx
@@ -1,77 +1,45 @@
-"use client";
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 import { AppSidebar } from "@/components/app-sidebar";
-import Link from "next/link";
-import { LogOut, User, UserCircle } from "lucide-react";
-
-import { Button } from "@/components/ui/button";
-import {
-	DropdownMenu,
-	DropdownMenuContent,
-	DropdownMenuGroup,
-	DropdownMenuItem,
-	DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
-import { Separator } from "@/components/ui/separator";
-import { logoutUser } from "@/lib/auth/auth";
-import { toast } from "sonner";
-import { useRouter } from "next/navigation";
+import MobileDock from "../../components/MobileDock";
+import ProfileDropdown from "../../components/ProfileDropDown";
+import LogoBtn from "../../components/LogoBtn";
+import { NoticeProvider } from "../providers";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
 	const role = "student";
 	return (
-		<SidebarProvider>
-			<AppSidebar role={role} />
-
-			<main className="flex-1 px-4 pb-4">
-				<div className="flex sticky z-50 top-0 py-4 bg-background items-center mb-4 gap-2">
-					<div className="flex items-center">
-						<SidebarTrigger />
-						<Link href={"/"}>FMU Mess</Link>
-					</div>
-					<div className="ml-auto">
-						<ProfileDropdown role={role} />
-					</div>
+		<NoticeProvider>
+			<div className="min-h-screen">
+				<div className="sm:hidden">
+					<main className="flex-1 px-4 py-24">
+						<div className="flex fixed w-screen h-20 z-50 top-0 left-0 p-4 bg-background items-center mb-4 gap-2 border-b">
+							<LogoBtn />
+							<div className="ml-auto">
+								<ProfileDropdown role={role} />
+							</div>
+						</div>
+						{children}
+						<MobileDock role={role} />
+					</main>
 				</div>
-				{children}
-			</main>
-		</SidebarProvider>
-	);
-}
 
-function ProfileDropdown({ role }: { role: string }) {
-	const router = useRouter();
-	const handleLogout = async () => {
-		await logoutUser()
-			.then((resp) => {
-				toast(`${resp.message}`);
-				router.push("/login");
-			})
-			.catch((err) => {
-				toast("Logout Successful!");
-				router.push("/login");
-			});
-	};
-	return (
-		<DropdownMenu>
-			<DropdownMenuTrigger asChild>
-				<Button variant="outline">
-					<UserCircle />
-				</Button>
-			</DropdownMenuTrigger>
-			<DropdownMenuContent align="end">
-				<DropdownMenuGroup>
-					<DropdownMenuItem onClick={() => router.push(`/${role}/profile`)}>
-						<User />
-						Profile
-					</DropdownMenuItem>
-					<Separator />
-					<DropdownMenuItem onClick={() => handleLogout()}>
-						<LogOut />
-						Logout
-					</DropdownMenuItem>
-				</DropdownMenuGroup>
-			</DropdownMenuContent>
-		</DropdownMenu>
+				{/* Desktop View */}
+				<div className="hidden sm:block">
+					<SidebarProvider>
+						<AppSidebar role={role} />
+						<main className="flex-1 px-4 pb-4">
+							<div className="flex sticky z-50 top-0 py-4 bg-background items-center mb-4 gap-2">
+								<SidebarTrigger />
+								<LogoBtn />
+								<div className="ml-auto">
+									<ProfileDropdown role={role} />
+								</div>
+							</div>
+							{children}
+						</main>
+					</SidebarProvider>
+				</div>
+			</div>
+		</NoticeProvider>
 	);
 }
