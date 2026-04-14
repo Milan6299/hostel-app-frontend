074fc98a2bd3e2df8ff8a8997cb8f5988ddde10f
Milan6299 <milanmahapatra140@gmail.com>
2026-04-14 13:02:41 +0530

    profile dropdown separated so resused in all pages

---
+++ components/ProfileDropDown.tsx
@@ -0,0 +1,52 @@
+"use client";
+import { LogOut, User, UserCircle } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import {
+	DropdownMenu,
+	DropdownMenuContent,
+	DropdownMenuGroup,
+	DropdownMenuItem,
+	DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Separator } from "@/components/ui/separator";
+import { logoutUser } from "@/lib/auth/auth";
+import { toast } from "sonner";
+import { useRouter } from "next/navigation";
+import { Role } from "@/lib/types";
+
+export default function ProfileDropdown({ role }: { role: Role }) {
+	const router = useRouter();
+	const handleLogout = async () => {
+		await logoutUser()
+			.then((resp) => {
+				toast(`${resp.message}`);
+				router.push("/login");
+			})
+			.catch((err) => {
+				toast("Logout Successful!");
+				router.push("/login");
+			});
+	};
+	return (
+		<DropdownMenu>
+			<DropdownMenuTrigger asChild>
+				<Button variant="outline">
+					<UserCircle />
+				</Button>
+			</DropdownMenuTrigger>
+			<DropdownMenuContent align="end">
+				<DropdownMenuGroup>
+					<DropdownMenuItem onClick={() => router.push(`/${role}/profile`)}>
+						<User />
+						Profile
+					</DropdownMenuItem>
+					<Separator />
+					<DropdownMenuItem onClick={() => handleLogout()}>
+						<LogOut />
+						Logout
+					</DropdownMenuItem>
+				</DropdownMenuGroup>
+			</DropdownMenuContent>
+		</DropdownMenu>
+	);
+}
